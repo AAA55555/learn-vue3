feeb65a5dbf329a32ac307eb1d416665eae4ae6b
Anton K <BulletAnton@yandex.ru>
2021-12-13 20:58:39 +0400

    8. Работа с запросами

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,120 @@
 <template>
   <div class="container">
-    <div class="card">
+    <app-alert :alert="alert" @close="alert = null"></app-alert>
+
+    <form class="card" @submit.prevent="createPerson">
       <h2>Работа с базой данных</h2>
-    </div>
+
+      <div class="form-control">
+        <label for="name">Введите имя</label>
+        <input type="text" id="name" v-model.trim="name">
+      </div>
+
+      <button class="btn primary" :disabled="name.length === 0">Создать человека</button>
+    </form>
+
+    <app-loader v-if="loading"></app-loader>
+
+    <app-people-list
+        v-else
+        :people="people"
+        @load="loadPeople"
+        @remove="removePerson"
+    ></app-people-list>
+
   </div>
 </template>
 
 <script>
+
+import AppPeopleList  from "./AppPeopleList";
+import axios from "axios";
+import AppAlert from "./AppAlert";
+import AppLoader from "./AppLoader";
+
 export default {
+  data() {
+    return {
+      name: '',
+      people: [],
+      alert: null,
+      loading: false
+    }
+  },
+  components: {
+    AppPeopleList,
+    AppAlert,
+    AppLoader
+  },
+  mounted() {
+    this.loadPeople()
+  },
+  methods: {
+    async createPerson () {
+      // https://vue-with-http-959f3-default-rtdb.firebaseio.com/people.json
+
+      const response = await fetch('https://vue-with-http-959f3-default-rtdb.firebaseio.com/people.json', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'createPerson'
+        },
+        body: JSON.stringify({
+          firstName: this.name
+        })
+      })
+
+      const firebaseData = await response.json()
+
+      this.people.push({
+        firstName: this.name,
+        id: firebaseData.name
+      })
+      this.name = ''
+    },
+    async loadPeople () {
+      try {
+        this.loading = true
+        const { data } = await axios.get('https://vue-with-http-959f3-default-rtdb.firebaseio.com/people.json')
+        if (!data) {
+          throw new Error('Список людей пуст')
+        }
+        this.people = Object.keys(data).map(key => {
+          return {
+            id: key,
+            // firstName: data[key].firstName
+            ...data[key]
+          }
+        })
+        this.loading = false
+      } catch (e) {
+        this.alert = {
+          type: 'danger',
+          title: 'Ошибка',
+          text: e.message
+        }
+        this.loading = false
+      }
+    },
+    async removePerson (id) {
+      try {
+        const person = this.people.find(person => person.id === id).firstName
+        await axios.delete(`https://vue-with-http-959f3-default-rtdb.firebaseio.com/people/${id}.json`)
+        this.people = this.people.filter(person => person.id !== id)
+        this.alert = {
+          type: 'primary',
+          title: 'Успешно',
+          text: `Пользователь с именем ${person} был удален`
+        }
+      } catch (e) {
+        console.log(5)
+      }
+
+    }
+  }
 }
 </script>
 
 <style>
 
 </style>
+
