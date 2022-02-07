4e07dcaaacdd12409a0f828571891df0b67d63b8
Anton Kondratev <kondratev@elma-bpm.com>
2022-02-07 21:49:24 +0400

    9. Mixin

---
+++ src/components/AppBlock.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <app-alert
+        v-if="alert"
+        text="Это очень важное сообщение"
+        title="Внимание!"
+        type="primary"
+        @close="alert = false"
+    />
+    <div class="card">
+      <button class="btn primary" @click="toggleAlert">{{ alert ? 'Скрыть' : 'Показать'}} сообщение</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import AppAlert from './AppAlert'
+import alertMixin from '../alertMixin'
+
+export default {
+  name: 'App',
+  mixins: [alertMixin],
+  components: {AppAlert}
+}
+</script>
+
