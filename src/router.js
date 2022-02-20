2e7659dc775f8242b5aaba4a56bc8cef60244276
Anton Kondratev <kondratev@elma-bpm.com>
2022-02-20 21:41:53 +0400

    10 Router

---
+++ src/router.js
@@ -0,0 +1,67 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Login from './views/Login'
+import Dashboard from './views/Dashboard'
+import Forget from './views/Forget'
+// import Mail from './views/Mail'
+import AppEmailBody from './components/AppEmailBody'
+import NotFound from './views/NotFound'
+
+const Mail = () => import('./views/Mail')
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/login',
+      component: Login,
+      alias: '/'
+    },
+    {
+      path: '/forget',
+      component: Forget,
+      meta: {
+        cantEnter: true
+      }
+    },
+    {
+      path: '/dashboard',
+      component: Dashboard,
+      name: 'home',
+      beforeEnter() {
+        console.log('beforeEnter')
+      }
+    }
+    ,
+    {
+      path: '/mail',
+      component: Mail,
+      name: 'email',
+      children: [
+        { path: ':mailId?',
+          component: AppEmailBody,
+          props: true }
+      ] //mail/123
+    },
+    {
+      path: '/:notFound(.*)',
+      component: NotFound
+    }
+  ],
+  linkActiveClass: 'active',
+  linkExactActiveClass: 'active'
+})
+
+router.beforeEach((to, from, next) => {
+  console.log('beforeEach')
+  if (to.meta.cantEnter) {
+    next({name: 'home'})
+  } else {
+    next()
+  }
+})
+
+router.afterEach((to, from) => {
+
+})
+
+export default router
---
+++ src/views/NotFound.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="card">
+    <h1>Страница не найдена</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium blanditiis culpa doloribus dolorum excepturi, ipsa minus molestiae nostrum, optio perspiciatis quibusdam quis repellendus saepe veniam voluptas! A fugiat libero voluptatem!</p>
+    <router-link to="/">Авторизоваться</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "notFound"
+}
+</script>
+
+<style scoped>
+</style>
