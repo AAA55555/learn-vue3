d885852d8e3852cfedb517d1f8c9fa050c877a4f
Anton Kondratev <kondratev@elma-bpm.com>
2022-03-08 21:22:32 +0400

    12 Composition Api. Composition - используется для больших проектов, чтобы удобней было использовать методы...

---
+++ src/views/HomePage.vue
@@ -0,0 +1,98 @@
+<template>
+<!--  <app-alert-->
+<!--      v-if="alert"-->
+<!--      title="Работаем с Composition"-->
+<!--      type="danger"-->
+<!--      @close="close"-->
+<!--  />-->
+  <div class="card">
+    <h1>Vue Composition Api</h1>
+    <small>data, methods, computed, watch</small>
+    <hr>
+
+    <div class="form-control">
+      <!--        <input type="text" ref="textInput">-->
+      <input type="text" v-model="firstName">
+    </div>
+
+    <button class="btn" @click="change">Изменить</button>
+  </div>
+  <FrameworkInfo @change-version="changeVersion"/>
+</template>
+
+<script>
+import FrameworkInfo from '../FrameworkInfo'
+import { ref,
+  reactive,
+  toRefs,
+  computed,
+  watch,
+  provide,
+  onBeforeMount,
+  onMounted,
+  onBeforeUpdate,
+  onUpdated,
+  onBeforeUnmount,
+  onUnmunted
+} from 'vue'
+export default {
+  name: "HomePage",
+  setup() {
+    const name = ref('VueJS')
+    const version = ref(3)
+    const textInput = ref(null)
+    const firstName = ref('')
+
+    const framevork = reactive({
+      name: 'VueJS',
+      version: 3
+    })
+
+    console.log('created')
+
+    onBeforeMount(() => {
+      console.log('onBeforeMount')
+    })
+
+    onMounted(() => {
+      console.log('onMounted')
+    })
+
+    onBeforeUpdate(() => {
+      console.log('onBeforeUpdate')
+    })
+
+    onUpdated(() => {
+      console.log('onUpdated')
+    })
+
+    // watch(doubleVersion, (newValue, oldValue) => {
+    //   console.log('old', oldValue)
+    //   console.log('new', newValue)
+    // })
+
+    function changeInfo() {
+      name.value = 'Vue JS !'
+      version.value = 42
+    }
+
+    function changeVersion(num) {
+      version.value = num
+    }
+
+    provide('name', name)
+    provide('version', version)
+
+    return {
+      change: changeInfo,
+      changeVersion,
+      firstName
+    }
+  },
+  components: {FrameworkInfo}
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/ReusablePage.vue
@@ -0,0 +1,56 @@
+<template>
+  <app-alert
+      v-if="alert"
+      title="Работаем с Composition"
+      type="primary"
+      @close="close"
+  />
+  <div class="card">
+    <h1>Переиспользование</h1>
+
+    <button class="btn primary" @click="toggle">{{alert ? 'Закрыть' : 'Показать'}} сообщение</button>
+    <button class="btn" @click="navigate">Перейти на главную</button>
+  </div>
+</template>
+
+<script>
+import {useRouter, useRoute} from 'vue-router'
+import AppAlert from '../AppAlert'
+import {reactive} from 'vue'
+import {useAlert} from '../use/alert'
+
+export default {
+  name: "ReusablePage",
+  setup() {
+    // const alert = reactive({
+    //   type: 'warning',
+    //   title: 'Reactive Alert'
+    // })
+    // const {alert, close, toggle} = useAlert()
+    // const [simpleAlert, toggle, close] = useAlert()
+
+    const router = useRouter()
+    const route = useRoute()
+
+    const navigate = () => {
+      router.push('/')
+    }
+
+    // return {
+    //   alert,
+    //   navigate,
+    //   toggle,
+    //   close
+    // }
+    return {
+      navigate,
+      ...useAlert()
+    }
+  },
+  components: {AppAlert}
+}
+</script>
+
+<style scoped>
+
+</style>
